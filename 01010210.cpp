7ecff3972a3eb6898f41c66bbb0a5b48316ea3a6
Dlmitriyyy <streltsowdm@gmail.com>
2023-10-14 17:09:19 +0300

    Добавьте файлы проекта.

---
+++ 01010210.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	system(" chcp 1251 > nul");
+	int cash, coins, cash_after, coins_after;
+
+	cout << "Введіть кількість гривень: ";
+	cin >> cash;
+
+	cout << "Введіть кількість копійок: ";
+	cin >> coins;
+
+	cash_after = cash + (coins / 100);
+	coins_after = coins % 100;
+
+	cout << cash_after << " гривень " << coins_after << " копійок.";
+
+	return 0;
+}
